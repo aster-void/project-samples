4bd7da9b4a4d0f52376db7f4039c6c5705bb054b
aster <137767097+aster-void@users.noreply.github.com>
2024-09-10 01:31:13 +0900

    make warp function

--- separate/backend/warp/src/main.rs
+++ separate/backend/warp/src/main.rs
@@ -2,9 +2,12 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let root = warp::get().and(
-        
-    )
+    let root = warp::any()
+        .and(warp::path::param::<String>())
+        .map(|s| format!("Hello from warp/{}! ", s))
+        .and(warp::path::param::<String>())
+        .map(|a, b| format!("Hello from warp/{}/{}! ", a, b));
+
+    println!("[log] Running warp application!");
+    warp::serve(root).run(([127, 0, 0, 1], 3000)).await;
 }
-
-
